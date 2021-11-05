a51fce9bc84b64279d88bc7da339e853b0fb25c9
alexeyk500 <alexeyk500@yandex.ru>
2021-11-06 00:27:59 +0300

    finally - done

--- src/App.tsx
+++ src/App.tsx
@@ -1,76 +1,79 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
-import {DragDropContext, Droppable, Draggable} from 'react-beautiful-dnd';
+import {DragDropContext, Droppable, Draggable, DropResult} from 'react-beautiful-dnd';
 
-const cardsList = [
+const list = [
   {
-    id: 0,
-    text: ' Card with id=0'
+    id: 'id_0',
+    text: ' Card with id = 0'
   },
   {
-    id: 1,
-    text: ' Card with id=1'
+    id: 'id_1',
+    text: ' Card with id = 1'
   },
   {
-    id: 2,
-    text: ' Card with id=2'
+    id: 'id_2',
+    text: ' Card with id = 2'
   },
   {
-    id: 3,
-    text: ' Card with id=3'
+    id: 'id_3',
+    text: ' Card with id = 3'
   },
   {
-    id: 4,
-    text: ' Card with id=4'
+    id: 'id_4',
+    text: ' Card with id = 4'
   },
-
 ]
 
 function App() {
 
-  const onDragEnd = () => {
+  const [cardsList, setCardsList] = useState(list)
 
+  const onDragEnd = (result: DropResult) => {
+    const newList = [...cardsList];
+    const [reorderedItem] = newList.splice(result.source.index, 1);
+    newList.splice(result.destination?.index!, 0, reorderedItem);
+    setCardsList(newList);
   }
 
   return (
     <div className={'container'}>
-        <DragDropContext
-          onDragEnd={onDragEnd}
-        >
-          <div className={'list_wrapper'}>
-            <Droppable droppableId={'list'}>
-            { (provided) => (
-                <div
-                  className={'list'}
-                  ref={provided.innerRef}
-                  {...provided.droppableProps}
-                >
-                  {
-                    cardsList.map((card, ind)=>{
-                      return(
-                        <Draggable  key = {ind} draggableId={ind+''} index={ind}>
-                          {(provided)=>(
-                            <div
-                              className={'card'}
-                              ref={provided.innerRef}
-                              {...provided.dragHandleProps}
-                              {...provided.draggableProps}
-                            >
-                              {card.id} - {card.text}
-                            </div>
-                          )}
-                        </Draggable>
-                      )
-                    })
-                  }
-                  {provided.placeholder}
-                </div>
-              )
-            }
+      <DragDropContext
+        onDragEnd={onDragEnd}
+      >
+        <div className={'list_wrapper'}>
+          <Droppable droppableId={'list'}>
+            {(provided) => (
+              <div
+                className={'list'}
+                ref={provided.innerRef}
+                {...provided.droppableProps}
+              >
+                {
+                  cardsList.map((card, ind) => {
+                    return (
+                      <Draggable key={ind} draggableId={ind + ''} index={ind}>
+                        {(provided) => (
+                          <div
+                            className={'card'}
+                            ref={provided.innerRef}
+                            {...provided.dragHandleProps}
+                            {...provided.draggableProps}
+                          >
+                            {card.id} - {card.text}
+                          </div>
+                        )}
+                      </Draggable>
+                    )
+                  })
+                }
+                {provided.placeholder}
+              </div>
+            )}
           </Droppable>
-          </div>
-        </DragDropContext>
-      </div>
+        </div>
+      </DragDropContext>
+    </div>
   );
 
 }
